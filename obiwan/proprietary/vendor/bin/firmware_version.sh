0c64aaa2eebf52f3c117d8959297686c9b4d13ad
AT WORLD <atharvsutar3102003@gmail.com>
2021-12-24 19:37:17 +0530

    vendor: Removed unwanted devices

--- obiwan/proprietary/vendor/bin/firmware_version.sh
+++
@@ -1,4 +0,0 @@
-#!/vendor/bin/sh
-
-BATTERY=`cat /sys/class/extcon/battery/name`
-setprop vendor.battery.version.driver "$BATTERY"
--- obiwan/proprietary/vendor/bin/init.asus.kernelmessage.sh
+++
@@ -1,9 +0,0 @@
-#!/vendor/bin/sh
-kernelmessage_flag=`getprop persist.vendor.asus.kernelmessage`
-is_oemuartlog_exist=`cat /proc/cmdline | grep userdebug`
-
-if test "$is_oemuartlog_exist"; then
-    echo 7 > /proc/sys/kernel/printk
-else
-    echo "$kernelmessage_flag" > /proc/sys/kernel/printk
-fi
